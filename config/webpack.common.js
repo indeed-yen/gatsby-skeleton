dbc56598f477054c19e3a84a839a70ae2928b9ec
ykuo@indeed.com <ykuo@indeed.com>
2018-11-01 17:13:14 -0500

    use FerJssilva biolerplate

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,65 +1,59 @@
-const path = require("path");
-const rootPath = path.resolve(__dirname, './../');
-const srcPath = path.resolve(rootPath, 'frontend');
-const BundleTracker = require('webpack-bundle-tracker');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-
-// the path(s) that should be cleaned
-const pathsToClean = [
-  path.resolve(rootPath, 'static/pack/'),
-];
-
-// the clean options to use
-const cleanOptions = {
-  root: path.resolve(rootPath),
-  verbose: true
-};
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: path.resolve(srcPath, 'entries/app.jsx'),
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all"
-    }
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(rootPath, 'static/pack/'),
-    publicPath : '/static/pack/'
-  },
-  plugins: [
-    new CleanWebpackPlugin(pathsToClean, cleanOptions),
-    new BundleTracker({filename: './webpack-stats.json'})
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['react']
-        }
-      },
-      {
-        test: /\.html/,
-        use: {
-          loader: 'html-loader',
-        }
-      },
-      {
-        test: /\.(png|jpg|svg|ttf|eot|woff|woff2|TTF|OTF)$/,
-        use: {
-          loader: 'url-loader?limit=8192&name=images/[name]-[hash:8].[ext]'
-        }
-      }
-    ]
-  },
-  resolve: {
-    modules: [srcPath, "node_modules"],
-    extensions: ['.js', '.jsx', '.css', '.scss', '.png', '.svg', '.jpeg', 'jpg']
-  }
-};
+    entry: {
+        main: path.resolve(__dirname, "../src", "index.js"),
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, '../dist'),
+        publicPath: "/"
+    },
+    devServer: {
+        port: 3042,
+        historyApiFallback: true,
+        overlay: true,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: [/node_modules/],
+                use: [{ loader: "babel-loader" }]
+            },
+            {
+                test: /.*\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'images/[name]_[hash:7].[ext]',
+                        }
+                    },
+                ]
+            },
+            {
+                test: /.*\.(gif|png|jp(e*)g)$/i,
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 10000,
+                            name: "images/[name].[ext]"
+                        }
+                    }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: path.resolve(__dirname, '../public', 'index.html'),
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+}
--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,33 +1,39 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = merge(common, {
-  devtool: 'cheap-eval-source-map',
-  mode: 'development',
-  watchOptions: { // for watch mode
-    ignored: /node_modules/
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s?css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('precss'),
-                  require('autoprefixer')
+module.exports = merge (common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: 3042,
+        historyApiFallback: true,
+        overlay: true,
+        open: true,
+        stats: 'errors-only'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader?sourceMap" }
                 ]
-              }
-            }
-          },
-          'sass-loader',
-        ],
-      }
-    ]
-  }
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    { loader: "style-loader" },
+                    { loader: "css-loader" },
+                    { loader: "sass-loader" }
+                ]
+            },
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash:8].css",
+        }),
+    ],
 });
